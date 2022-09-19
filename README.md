9963629d9df308bc8ffd6a6e24e00626cd7f8edf
recoilme <vadim-kulibaba@yandex.ru>
2022-09-19 12:00:29 +0300

    pocketbase

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Work In Progress
+
+## Build
+ - use go 1.19 (1.18 must work to)
+ - go build -o vsd && ./vsd serve --debug
+ - admin ui http://127.0.0.1:8090/_/
--- main.go
+++ main.go
@@ -1,5 +1,29 @@
-package verysmartdog
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/labstack/echo/v5"
+	"github.com/pocketbase/pocketbase"
+	"github.com/pocketbase/pocketbase/core"
+)
 
 func main() {
+	app := pocketbase.New()
+	app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
+		// add new "GET /api/hello" route to the app router (echo)
+		e.Router.AddRoute(echo.Route{
+			Method: http.MethodGet,
+			Path:   "",
+			Handler: func(c echo.Context) error {
+				return c.String(200, "Hello world!")
+			},
+		})
 
+		return nil
+	})
+	if err := app.Start(); err != nil {
+		log.Fatal(err)
+	}
 }
